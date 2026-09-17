156958e6bfeef1cee9c6f8e0c8dfcfe3b76bdf57
shewnaethan <ethan29024@gmail.com>
2026-09-16 21:28:28 -0500

    some small fixes v1.01

--- scripts/windchill.js
+++ scripts/windchill.js
@@ -4,21 +4,15 @@ let windSpeed = 5;
 
 function calcWindChill(temp, wind) {
     let windChillTemp = ""
-    if (temp <= 50 && wind >= 3) {
-        windChillTemp = (35.74 + (0.6125 * temp) - (35.75 * (wind * 0.16) + (0.4275 * temp * (wind * 0.16))))
-        windChillTemp = windChillTemp.toFixed(1);
-    }
-    else {
-        windChillTemp = "N/A";
-    }
+    windChillTemp = (35.74 + (0.6125 * temp) - (35.75 * (wind * 0.16) + (0.4275 * temp * (wind * 0.16))))
+    windChillTemp = windChillTemp.toFixed(1);
     return windChillTemp;
 };
 
-let chill = calcWindChill(tempature, windSpeed);
-
 if (tempature <= 50 && windSpeed >= 3) {
+    let chill = calcWindChill(tempature, windSpeed);
     windChill.innerHTML = `${chill} °F`;
 }
 else {
-    windChill.innerHTML = `${chill}`
+    windChill.innerHTML = `N/A`
 }
